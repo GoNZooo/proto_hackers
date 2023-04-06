7aff3fd92bae1fbc57be90669ca862f28b8d513c
Rickard Andersson <gonz@severnatazvezda.com>
2023-04-06 15:35:31 +0300

    refactor(SimpleGenServer): use record for server state

--- src/SimpleGenServer.erl
+++ src/SimpleGenServer.erl
@@ -3,6 +3,11 @@
 -export([startLink_/2, cast/2, call/2, init/1, handle_info/2, handle_cast/2,
          handle_call/3]).
 
+-record(state, {state :: term(), handleInfo :: handle_info_function()}).
+
+-type handle_info_function() :: fun((term(), term()) -> handle_info_return()).
+-type handle_info_return() :: {simpleNoReply, term()} | {simpleStop, term(), term()}.
+
 startLink_(StartArguments,
            #{init := Init,
              handleInfo := HandleInfo,
@@ -37,33 +42,33 @@ call(Pid, F) ->
 init({StartArguments, Init, HandleInfo}) ->
   case (Init(StartArguments))() of
     {simpleInitOk, State} ->
-      {ok, #{state => State, handleInfo => HandleInfo}};
+      {ok, #state{state = State, handleInfo = HandleInfo}};
     {simpleInitError, Foreign} ->
       {stop, Foreign}
   end.
 
-handle_info(Message, #{state := State, handleInfo := HandleInfo} = S) ->
+handle_info(Message, #state{state = State, handleInfo = HandleInfo} = ServerState) ->
   case ((HandleInfo(Message))(State))() of
     {simpleNoReply, NewState} ->
-      {noreply, S#{state => NewState}};
+      {noreply, ServerState#state{state = NewState}};
     {simpleStop, Reason, NewState} ->
-      {stop, translate_stop_reason(Reason), S#{state => NewState}}
+      {stop, translate_stop_reason(Reason), ServerState#state{state = NewState}}
   end.
 
-handle_cast({cast, F}, #{state := State} = S) ->
+handle_cast({cast, F}, #state{state = State} = ServerState) ->
   case (F(State))() of
     {simpleNoReply, NewState} ->
-      {noreply, S#{state => NewState}};
+      {noreply, ServerState#state{state = NewState}};
     {simpleStop, Reason, NewState} ->
-      {stop, translate_stop_reason(Reason), S#{state => NewState}}
+      {stop, translate_stop_reason(Reason), ServerState#state{state = NewState}}
   end.
 
-handle_call(From, {call, F}, #{state := State} = S) ->
+handle_call(From, {call, F}, #state{state = State} = ServerState) ->
   case ((F(From))(State))() of
     {simpleReply, Reply, NewState} ->
-      {reply, Reply, S#{state => NewState}};
+      {reply, Reply, ServerState#state{state = NewState}};
     {simpleStop, Reason, NewState} ->
-      {stop, translate_stop_reason(Reason), S#{state => NewState}}
+      {stop, translate_stop_reason(Reason), ServerState#state{state = NewState}}
   end.
 
 translate_stop_reason({stopNormal}) ->
